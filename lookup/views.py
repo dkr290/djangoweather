38df68d96de0eade7d6dbcb936abb99707336b3a
Danail Krasimirov <danidhcp@bv.bg>
2020-01-13 22:25:26 +0100

    change of views and css html

--- lookup/views.py
+++ lookup/views.py
@@ -2,11 +2,22 @@
 # this is my views file
 from django.shortcuts import render
 
+
 # Create your views here.
 
-
 def home(request):
-    return render(request,"home.html", {})
+    import json
+    import requests
+
+    api_request = requests.get("http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=89129&distance=5&API_KEY=EF02C6A5-A70D-4161-89E4-14F219CFC0B3")
+    
+    try:
+        api = json.loads(api_request.content)
+    except Exception as e:
+        api = "Error..."
+      
+
+    return render(request,"home.html", {'api': api})
 
 
 
